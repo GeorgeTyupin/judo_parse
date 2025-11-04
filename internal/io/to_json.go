d6bcd7bf43a5da1c7ac52f8e2e0d962e5cade410
GeorgeTyupin <GeorgeTyupin@ya.ru>
2025-11-04 22:57:09 +0300

    refactor: Обновил правила транслитерации

--- internal/io/to_json.go
+++ internal/io/to_json.go
@@ -12,7 +12,6 @@ import (
 
 func ToJson(wg *sync.WaitGroup, data models.ExelSheet, file string) {
 	defer wg.Done()
-	fmt.Println(1)
 
 	newJson, err := os.Create(fmt.Sprintf("%s.json", file))
 	if err != nil {
