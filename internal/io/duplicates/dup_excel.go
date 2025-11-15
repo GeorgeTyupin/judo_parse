fd9ef5a3438ed4b77c26c6431bed1b2d90bf8a9e
GeorgeTyupin <GeorgyTyupin@ya.ru>
2025-11-15 20:25:53 +0300

    refactor: Рефакторинг модуля поиска дублей: реализована базовая логика обхода данных и сохранения записей в duplicates.go, удален файл с типами дублей. Упрощена структура DuplicateTable и добавлена обертка DuplicateNote для работы с дублями в dup_excel.go. Метод сохранения записи SaveNote перемещен из PivotTable в общую модель Note в models.go для переиспользования. Вспомогательные функции SafeGet и FormatDate сделаны публичными в to_excel.go для использования в других пакетах.

--- internal/io/duplicates/dup_excel.go
+++ internal/io/duplicates/dup_excel.go
@@ -2,15 +2,15 @@ package dupio
 
 import (
 	"fmt"
+
 	"judo/internal/models"
 
 	"github.com/xuri/excelize/v2"
 )
 
 type DuplicateTable struct {
-	Name   string
-	Table  *excelize.File
-	Sheets []string
+	Name  string
+	Table *excelize.File
 }
 
 func InitTable(name string) *DuplicateTable {
@@ -19,12 +19,6 @@ func InitTable(name string) *DuplicateTable {
 	dTable := DuplicateTable{
 		Name:  name,
 		Table: table,
-		Sheets: []string{
-			"Тип 1",
-			"Тип 2",
-			"Тип 3",
-			"Тип 4",
-		},
 	}
 
 	dTable.setHeader()
@@ -33,10 +27,27 @@ func InitTable(name string) *DuplicateTable {
 }
 
 func (d *DuplicateTable) setHeader() {
-	for _, sheet := range d.Sheets {
-		for i, header := range models.Headers {
-			cell := fmt.Sprintf("%c1", 'A'+i)
-			d.Table.SetCellValue(sheet, cell, header)
-		}
+	for i, header := range models.Headers {
+		cell := fmt.Sprintf("%c1", 'A'+i)
+		d.Table.SetCellValue("Sheet1", cell, header)
 	}
+}
+
+type DuplicateNote struct {
+	Note          models.Note
+	DuplicateType string
+}
+
+func (dupNote *DuplicateNote) SaveDuplicateNote(table *excelize.File, rowIndex int) {
+	rowNum := rowIndex + 2
+
+	dupNote.Note.SaveNote(table, rowIndex)
+	table.SetCellValue("Sheet1", fmt.Sprintf("W%d", rowNum), dupNote.DuplicateType)
+}
+
+func (d *DuplicateTable) SaveTable() {
+	if err := d.Table.SaveAs(fmt.Sprintf("%s.xlsx", d.Name)); err != nil {
+		fmt.Printf("Ошибка сохранения файла %s: %v\n", d.Name, err)
+	}
+	d.Table.Close()
 }
--- internal/models/models.go
+++ internal/models/models.go
@@ -1,5 +1,11 @@
 package models
 
+import (
+	"fmt"
+
+	"github.com/xuri/excelize/v2"
+)
+
 type Judoka struct {
 	Rank      string `json:"RANK"`
 	Name      string `json:"NAME"`
@@ -42,6 +48,33 @@ type Note struct {
 	COUNTRY        string
 	COUNTRY_LAST   string
 	SO             string
+}
+
+func (note *Note) SaveNote(table *excelize.File, counter int) {
+	rowNum := counter + 2
+
+	table.SetCellValue("Sheet1", fmt.Sprintf("A%d", rowNum), note.TOURNAMENT)
+	table.SetCellValue("Sheet1", fmt.Sprintf("B%d", rowNum), note.TOUR_TYPE)
+	table.SetCellValue("Sheet1", fmt.Sprintf("C%d", rowNum), note.TOUR_PLACE)
+	table.SetCellValue("Sheet1", fmt.Sprintf("D%d", rowNum), note.TOUR_CITY)
+	table.SetCellValue("Sheet1", fmt.Sprintf("E%d", rowNum), note.TOUR_COUNTRY)
+	table.SetCellValue("Sheet1", fmt.Sprintf("F%d", rowNum), note.TOUR_CITY_LAST)
+	table.SetCellValue("Sheet1", fmt.Sprintf("G%d", rowNum), note.DATE)
+	table.SetCellValue("Sheet1", fmt.Sprintf("H%d", rowNum), note.YEAR)
+	table.SetCellValue("Sheet1", fmt.Sprintf("I%d", rowNum), note.MONTH)
+	table.SetCellValue("Sheet1", fmt.Sprintf("J%d", rowNum), note.GENDER)
+	table.SetCellValue("Sheet1", fmt.Sprintf("K%d", rowNum), note.WeightCategory)
+	table.SetCellValue("Sheet1", fmt.Sprintf("L%d", rowNum), note.WC)
+	table.SetCellValue("Sheet1", fmt.Sprintf("M%d", rowNum), note.RANK)
+	table.SetCellValue("Sheet1", fmt.Sprintf("N%d", rowNum), note.NAME)
+	table.SetCellValue("Sheet1", fmt.Sprintf("O%d", rowNum), note.FIRSTNAME)
+	table.SetCellValue("Sheet1", fmt.Sprintf("P%d", rowNum), note.JUDOKA)
+	table.SetCellValue("Sheet1", fmt.Sprintf("Q%d", rowNum), note.NAME_RUS)
+	table.SetCellValue("Sheet1", fmt.Sprintf("R%d", rowNum), note.FIRSTNAME_RUS)
+	table.SetCellValue("Sheet1", fmt.Sprintf("S%d", rowNum), note.JUDOKA_RUS)
+	table.SetCellValue("Sheet1", fmt.Sprintf("T%d", rowNum), note.COUNTRY)
+	table.SetCellValue("Sheet1", fmt.Sprintf("U%d", rowNum), note.COUNTRY_LAST)
+	table.SetCellValue("Sheet1", fmt.Sprintf("V%d", rowNum), note.SO)
 }
 
 // Headers defines the Excel column headers matching Note struct fields
