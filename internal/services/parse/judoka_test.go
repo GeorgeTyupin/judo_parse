7fd7a447766b4ae0860deddced9f50637b1daf2a
GeorgeTyupin <GeorgyTyupin@ya.ru>
2026-04-15 18:41:59 +0300

    test: добавил тест для JudokaService.Parse

---
+++ internal/services/parse/judoka_test.go
@@ -0,0 +1,92 @@
+package parse
+
+import (
+	"testing"
+
+	judoio "judo/internal/io/excel/judoka_parse"
+	"judo/internal/models"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestJudokasParsing(t *testing.T) {
+	reader, err := judoio.NewReader("../../../#JUDOKA.xlsx")
+	require.NoError(t, err)
+
+	service := NewJudokaService(reader)
+
+	judokas, err := service.Parse()
+	require.NoError(t, err)
+	require.NotEmpty(t, judokas)
+
+	testJudokas := []models.JudokaDBRow{
+		{
+			LastName:     "A. Almeida",
+			FirstName:    "Miguel",
+			LastNameRus:  new("A. Almeida"),
+			FirstNameRus: new("Miguel"),
+		},
+		{
+			LastName:     "Aaltonen",
+			FirstName:    "Tiina",
+			LastNameRus:  new("Aaltonen"),
+			FirstNameRus: new("Tiina"),
+		},
+		{
+			LastName:     "Aamodt",
+			FirstName:    "Alexander",
+			LastNameRus:  new("Aamodt"),
+			FirstNameRus: new("Alexander"),
+		},
+		{
+			LastName:     "Aarts",
+			FirstName:    "Monique",
+			LastNameRus:  new("Aarts"),
+			FirstNameRus: new("Monique"),
+		},
+		{
+			LastName:     "Abadie",
+			FirstName:    "",
+			LastNameRus:  new("Abadie"),
+			FirstNameRus: new(""),
+		},
+		{
+			LastName:     "Abanoz",
+			FirstName:    "Salim",
+			LastNameRus:  new("Abanoz"),
+			FirstNameRus: new("Salim"),
+		},
+		{
+			LastName:     "Abbad",
+			FirstName:    "Tahar",
+			LastNameRus:  new("Abbad"),
+			FirstNameRus: new("Tahar"),
+		},
+		{
+			LastName:     "Abdoune",
+			FirstName:    "Hamid",
+			LastNameRus:  new("Abdoune"),
+			FirstNameRus: new("Hamid"),
+		},
+		{
+			LastName:     "Abe",
+			FirstName:    "Takahiro",
+			LastNameRus:  new("Abe"),
+			FirstNameRus: new("Takahiro"),
+		},
+		{
+			LastName:     "Abe",
+			FirstName:    "Yuji",
+			LastNameRus:  new("Abe"),
+			FirstNameRus: new("Yuji"),
+		},
+	}
+
+	for i, expected := range testJudokas {
+		require.Equal(t, expected.LastName, judokas[i].LastName)
+		require.Equal(t, expected.FirstName, judokas[i].FirstName)
+		require.Equal(t, expected.LastNameRus, judokas[i].LastNameRus)
+		require.Equal(t, expected.FirstNameRus, judokas[i].FirstNameRus)
+	}
+
+}
